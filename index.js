0605185f1cc22d862747a9f5d0d3fb11d0bdfd87
Anjani Kumar <99578509+ANJani9web@users.noreply.github.com>
2023-01-30 11:16:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// ++a will first increament the value of 'a' then print the value of a
+// a++ will increament the value of 'a' but will print the non increament value of 'a'
+
+
+// == simple checks the value of both the operators
+// === checks the value and also the data type of the operators
